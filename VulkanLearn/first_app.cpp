e88abc8f4c2e5e8c7534578bc0b69fc1abc3687c
Rendi . <artemrepin00@gmail.com>
2024-04-18 20:50:26 +0300

    After playing with colors and depth

--- VulkanLearn/first_app.cpp
+++ VulkanLearn/first_app.cpp
@@ -75,7 +75,7 @@ namespace lve
 			render_pass_info.renderArea.extent = lve_swap_chain.getSwapChainExtent();
 			
 			std::array<VkClearValue, 2> clear_values{};
-			clear_values[0].color = {0.0f, 0.0f, 0.0f, 1.0f};
+			clear_values[0].color = {0.05f, 0.05f, 0.05f, 1.0f};
 			clear_values[1].depthStencil = { 1.0f, 0 };
 
 			render_pass_info.clearValueCount = static_cast<uint32_t>(clear_values.size());
